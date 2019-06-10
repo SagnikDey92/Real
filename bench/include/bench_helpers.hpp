ea411be7ec0e6d9eecfe3fe861a1976260ccacf5
SagnikDey92 <mail.sagnik.dey@gmail.com>
2019-06-10 17:22:53 +0000

    fix messed up rebase

--- bench/include/bench_helpers.hpp
+++ bench/include/bench_helpers.hpp
@@ -4,10 +4,7 @@
 
 #include <real/real.hpp>
 
-<<<<<<< HEAD
 template<> std::optional<unsigned int> boost::real::real<>::maximum_precision = 10;
 template<> std::optional<unsigned int> boost::real::real_algorithm<>::maximum_precision = 10;
-=======
->>>>>>> Added division, but it still needs work
 
 #endif //BOOST_REAL_BENCH_HELPERS_HPP
